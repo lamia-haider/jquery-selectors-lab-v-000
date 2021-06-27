8405dd7de1faa74090fddb93fc7715149ef1094b
Lamia Haider <lamia.haider@gmail.com>
2021-06-27 09:37:26 -0400

    commit tree 951d5412f93dc34c1f473132fd88e82aa4954515

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 'use-strict';
+
+// selects all <h1> elements and returns them using an element selector.
+
+function h1Selector(){
+  return $('h1');
+}
+
+// selects and returns all <li> inside of <ol> using a descendant selector.
+function liInOlSelector(){
+  return $('ol li');
+}
+
+// selects and returns the <a> that is inside of the element that has both an id of box4 and a class of box5.
+function linkSelector(){
+  return $('#box4.box5 a');
+}
+//that selects and returns the image of the cat that has the alt text equal to "cat sleeping".
+function imageSelector(){
+  return $('img[alt="cat sleeping"]');
+}
+//selects and returns all the checkbox inputs
+function checkboxInputSelector(){
+  return $('input[type="checkbox"]');
+}
